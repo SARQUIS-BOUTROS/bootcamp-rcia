17fdbc83f9d7046efb9560cb6b333e0d24dbda9e
lorenm <lorenamalina@gmail.com>
2018-10-16 10:11:44 -0300

    Calculator layout and function

---
+++ 05-js-basico/Lorena_Malina/c.js
@@ -0,0 +1,8 @@
+function retornar (num) {
+	var anterior document.fo.valores.value;
+	document.getElementById('valores').value=interior+num;
+}
+
+function eliminar_todo () {
+	document.fo.valores.value="";
+}
